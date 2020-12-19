cce17fb8cb59ebe78fb6d4c8dcd08172b80bde44
Sujeet Kumar <sujeet.sarkar@hotmail.com>
2020-12-20 00:58:40 +0530

    created components for nested route

--- src/app/authentication/authentication-routing.module.ts
+++ src/app/authentication/authentication-routing.module.ts
@@ -1,10 +1,27 @@
 import { Routes } from "@angular/router";
 import { PageNotFoundComponentComponent } from "../components/shared/page-not-found-component/page-not-found-component.component";
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { ChildAComponent } from "./components/first/child-a/child-a.component";
+import { ChildBComponent } from "./components/first/child-b/child-b.component";
+import { FirstComponent } from "./components/first/first.component";
 
 
 export const authRoutes: Routes = [
     { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
     { path: 'dashboard', component: DashboardComponent },
+    {
+        path: 'first-component',
+        component: FirstComponent, // this is the component with the <router-outlet> in the template
+        children: [
+          {
+            path: 'child-a', // child route path
+            component: ChildAComponent, // child route component that the router renders
+          },
+          {
+            path: 'child-b',
+            component: ChildBComponent, // another child route component that the router renders
+          },
+        ],
+      },
     { path: '**', component: PageNotFoundComponentComponent }
 ]
--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { authRoutes } from './authentication-routing.module';
 import { RouterModule } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FirstComponent } from './components/first/first.component';
+import { ChildAComponent } from './components/first/child-a/child-a.component';
+import { ChildBComponent } from './components/first/child-b/child-b.component';
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, FirstComponent, ChildAComponent, ChildBComponent],
   exports: [
       DashboardComponent,
       RouterModule
