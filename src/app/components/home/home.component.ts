2c8a04d7cb565f94760e67f3aaedc16b032126ed
Sujeet Kumar <sujeet.sarkar@hotmail.com>
2020-12-19 23:25:35 +0530

    removed dead code

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-home',
