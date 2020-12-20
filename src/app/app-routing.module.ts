7ebc098d611003d36e8e31c79a0d6345c90f795d
Sujeet Kumar <sujeet.sarkar@hotmail.com>
2020-12-20 18:05:28 +0530

    added servers component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { ServersComponent } from './components/servers/servers.component';
 import { PageNotFoundComponentComponent } from './components/shared/page-not-found-component/page-not-found-component.component';
 
 
@@ -9,6 +10,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'servers', component: ServersComponent },
   { path: 'auth', redirectTo: 'authentication', pathMatch: 'full'},
   { path: 'authentication', loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule)},
   { path: '**', component: PageNotFoundComponentComponent }
--- src/app/components/server/server.component.ts
+++ src/app/components/server/server.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server',
+  // selector: '[app-server]', // attribute selector
+  // selector: '.app-server', //class selector
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.css']
 })
