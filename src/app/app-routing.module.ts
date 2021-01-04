fd324c7525ceb4a7624cfe9db148fa51b8a7c5b8
Sujeet Kumar <sujeet.sarkar@hotmail.com>
2021-01-04 20:23:59 +0530

    adding route see previous commit for @Input

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { ParentComponent } from './components/parent/parent.component';
 import { ServersComponent } from './components/servers/servers.component';
 import { PageNotFoundComponentComponent } from './components/shared/page-not-found-component/page-not-found-component.component';
 
@@ -9,6 +10,7 @@ import { PageNotFoundComponentComponent } from './components/shared/page-not-fou
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'parent', component: ParentComponent },
   { path: 'login', component: LoginComponent },
   { path: 'servers', component: ServersComponent },
   { path: 'auth', redirectTo: 'authentication', pathMatch: 'full'},
